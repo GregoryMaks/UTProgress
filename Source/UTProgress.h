bb1647dd3958c237349f1ffd4df1c2202141fcfd
GregoryM <feedback.gm@gmail.com>
2014-03-27 18:02:05 +0200

    Added UTProgress classes

---
+++ Source/UTProgress.h
@@ -0,0 +1,51 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+
+UTProgress.h
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Gregory Maksyuk (GregoryMaks)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Interface
+
+// Serves as copy of Apple NSProgress but with some enhancements and also some minor issues
+@interface UTProgress : NSObject
+
+@property (nonatomic, assign) NSUInteger totalUnitCount;
+@property (nonatomic, assign) NSUInteger completedUnitCount;
+
+@property (nonatomic, weak) NSString *bubblingMessage; ///< message that bubbles up to root progress bar
+@property (nonatomic, weak) NSString *developerBubblingMessage; ///< message intended for dev usage (bubbles up)
+
+/// KVO observable property
+@property (nonatomic, assign, readonly) CGFloat fractionCompleted;
+
++ (instancetype)rootProgressWithTotalUnitCount:(NSUInteger)aTotalUnitCount;
++ (instancetype)progressWithParent:(UTProgress *)aParent totalUnitCount:(NSUInteger)aTotalUnitCount;
+
+/// @param aParent can be nil or parent progress set as current prior to calling this method
+- (id)initWithParent:(UTProgress *)aParent totalUnitCount:(NSUInteger)aTotalUnitCount;
+
+/// Becomes progress to which child progresses can be added
+- (void)becomeCurrentWithPendingUnitCount:(NSUInteger)aPendingUnitCount;
+- (void)resignCurrent;
+
+/// Can be called to mark progress as 100% completed
+- (void)completeProgress;
+
+@end
---
+++ Source/UTProgress.m
@@ -0,0 +1,248 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+
+UTProgress.m
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Gregory Maksyuk (GregoryMaks)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Imports
+
+#import "UTProgress.h"
+#import "UTProgressGroup.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Private interface
+
+@interface UTProgress ()
+{
+    NSUInteger _totalUnitCount;
+    NSUInteger _completedUnitCount;
+}
+
+@property (nonatomic, strong) UTProgress *parent;
+@property (nonatomic, strong) NSMutableArray *groups;
+
+@property (nonatomic, assign) BOOL isCurrent;
+@property (nonatomic, strong) UTProgressGroup *currentGroup;
+
+@property (nonatomic, assign) CGFloat fractionCompleted;
+
+- (void)recalculateFractionCompleted;
+
+- (void)addChild:(UTProgress *)aChildProgress;
+
+@end
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Implementation
+
+@implementation UTProgress
+
+- (NSUInteger)totalUnitCount
+{
+    @synchronized(self)
+    {
+        return _totalUnitCount;
+    }
+}
+- (void)setTotalUnitCount:(NSUInteger)totalUnitCount
+{
+    @synchronized(self)
+    {
+        if (totalUnitCount != _totalUnitCount)
+        {
+            _totalUnitCount = totalUnitCount;
+            [self recalculateFractionCompleted];
+        }
+    }
+}
+
+- (NSUInteger)completedUnitCount
+{
+    @synchronized(self)
+    {
+        return _completedUnitCount;
+    }
+}
+- (void)setCompletedUnitCount:(NSUInteger)completedUnitCount
+{
+    @synchronized(self)
+    {
+        if (completedUnitCount != _completedUnitCount)
+        {
+            _completedUnitCount = completedUnitCount;
+            [self recalculateFractionCompleted];
+        }
+    }
+}
+
++ (instancetype)rootProgressWithTotalUnitCount:(NSUInteger)aTotalUnitCount
+{
+    return [[[self class] alloc] initWithParent:nil totalUnitCount:aTotalUnitCount];
+}
+
++ (instancetype)progressWithParent:(UTProgress *)aParent totalUnitCount:(NSUInteger)aTotalUnitCount
+{
+    return [[[self class] alloc] initWithParent:aParent totalUnitCount:aTotalUnitCount];
+}
+
+- (id)initWithParent:(UTProgress *)aParent totalUnitCount:(NSUInteger)aTotalUnitCount
+{
+    if (self = [super init])
+    {
+        self.groups = [NSMutableArray array];
+        self.isCurrent = NO;
+        
+        if (aParent != nil)
+        {
+            if (!aParent.isCurrent)
+            {
+                [NSException raise:@"Logic" format:@"Parent progress should be made current before adding children.\
+Use [parent becomeCurrentWithPendingUnitCount]."];
+            }
+            
+            self.parent = aParent;
+            [self.parent addChild:self];
+        }
+        
+        self.totalUnitCount = aTotalUnitCount;
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    for (UTProgressGroup *group in self.groups)
+    {
+        for (UTProgress *child in group.children)
+        {
+            [child removeObserver:self forKeyPath:@"fractionCompleted"];
+        }
+    }
+}
+
+- (NSString *)description
+{
+    return [NSString stringWithFormat:@"Progress %p - <fractionCompleted: %.2f | completedUnits: %u | totalUnits: %u>",
+            self,
+            self.fractionCompleted,
+            self.completedUnitCount,
+            self.totalUnitCount];
+}
+
+- (void)becomeCurrentWithPendingUnitCount:(NSUInteger)aPendingUnitCount
+{
+    self.isCurrent = YES;
+    
+    // Create new group to add children to
+    self.currentGroup = [[UTProgressGroup alloc] init];
+    self.currentGroup.pendingUnitCount = aPendingUnitCount;
+    [self.groups addObject:self.currentGroup];
+}
+
+- (void)resignCurrent
+{
+    self.isCurrent = NO;
+    
+    self.currentGroup = nil;
+}
+
+- (void)completeProgress
+{
+    [self.groups removeAllObjects];
+    self.completedUnitCount = self.totalUnitCount;
+    [self recalculateFractionCompleted];
+}
+
+#pragma mark -
+#pragma mark Private methods
+
+- (void)addChild:(UTProgress *)aChildProgress
+{
+    if (!self.isCurrent || self.currentGroup == nil)
+    {
+        NSAssert(NO, @"");
+        return;
+    }
+    
+    [self.currentGroup.children addObject:aChildProgress];
+    
+    [aChildProgress addObserver:self forKeyPath:@"fractionCompleted" options:NSKeyValueObservingOptionNew context:NULL];
+    [aChildProgress addObserver:self forKeyPath:@"bubblingMessage" options:NSKeyValueObservingOptionNew context:NULL];
+    [aChildProgress addObserver:self forKeyPath:@"developerBubblingMessage" options:NSKeyValueObservingOptionNew context:NULL];
+}
+
+- (void)recalculateFractionCompleted
+{
+    CGFloat fractionPerUnit = 1.0 / self.totalUnitCount;
+    
+    CGFloat groupFractionsSum = 0;  // holds summary of all fractions from all groups (will be
+    //  (group_pending_units / total_units) * valid_group_count if 100% completed)
+    
+    NSUInteger totalGroupUnits = 0;
+    if (self.groups.count > 0)
+    {
+        for (UTProgressGroup *group in self.groups)
+        {
+            if (group.children.count != 0)  // check only valid groups
+            {
+                totalGroupUnits += group.pendingUnitCount;
+            }
+        }
+        
+        for (UTProgressGroup *group in self.groups)
+        {
+            if (group.children.count != 0)  // check only valid groups
+            {
+                CGFloat childFractionSum = 0;   // holds summary of all children fractions (will be 1.0 * child_count if 100% completed)
+                for (UTProgress *child in group.children)
+                {
+                    childFractionSum += [child fractionCompleted];
+                }
+                
+                CGFloat fractionPerGroup = fractionPerUnit * (CGFloat)group.pendingUnitCount;
+                groupFractionsSum += fractionPerGroup * (childFractionSum / (CGFloat)group.children.count);
+            }
+        }
+    }
+    
+    self.fractionCompleted = (fractionPerUnit * self.completedUnitCount) +
+        ((self.groups.count == 0) ? 0 : groupFractionsSum);
+}
+
+#pragma mark -
+#pragma mark KVO
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    if ([keyPath isEqual:@"fractionCompleted"])
+    {
+        [self recalculateFractionCompleted];
+    }
+    else if ([keyPath isEqual:@"bubblingMessage"])
+    {
+        self.bubblingMessage = [object bubblingMessage];
+    }
+    else if ([keyPath isEqual:@"developerBubblingMessage"])
+    {
+        self.developerBubblingMessage = [object developerBubblingMessage];
+    }
+}
+
+@end
